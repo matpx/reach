1be3b4d13e48efcac048ed5c1836c57afc9b4268
matpx <9219741+matpx@users.noreply.github.com>
2024-03-29 18:39:27 +0100

    fmt

--- src/manager/material_manager.cpp
+++ src/manager/material_manager.cpp
@@ -22,17 +22,14 @@ MaterialManager::MaterialManager() {
     unlit_pipeline_desc.shader = unlit_shader;
     unlit_pipeline_desc.index_type = SG_INDEXTYPE_UINT32;
     unlit_pipeline_desc.cull_mode = SG_CULLMODE_BACK;
-    unlit_pipeline_desc.depth = {
-        .compare = SG_COMPAREFUNC_LESS_EQUAL,
-        .write_enabled = true,
-    };
+    unlit_pipeline_desc.depth.compare = SG_COMPAREFUNC_LESS_EQUAL;
+    unlit_pipeline_desc.depth.write_enabled = true;
     unlit_pipeline_desc.label = "unlit pipeline";
 
     unlit_material.pipeline = sg_make_pipeline(unlit_pipeline_desc);
     POSTCONDITION(unlit_material.pipeline.id != SG_INVALID_ID);
 
-    unlit_material.uniform_transform_slot =
-        static_cast<uint8_t>(unlit_uniformblock_slot(SG_SHADERSTAGE_VS, "transform_params"));
+    unlit_material.uniform_transform_slot = static_cast<uint8_t>(unlit_uniformblock_slot(SG_SHADERSTAGE_VS, "transform_params"));
 
     self = this;
 }
